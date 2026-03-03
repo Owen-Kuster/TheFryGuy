800818494da12f63e321e918d760c4854398a76c
Owen Kuster <owenkuster08@gmail.com>
2026-03-03 10:27:21 +0100

    Added PHP lines and final design website

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style.css">
-        <title>Document</title>
-    </head>
-    <body>
-        <header>
-            <nav>
 
-            </nav>
-        </header>
-        <main>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>owens website</title>
+</head>
 
-        </main>
-        <footer>
+<body>
+    <header>
+        <nav>
 
-        </footer>
-    </body>
+        </nav>
+    </header>
+    <main>
+        <h1>we gaan hieronder php proberen</h1>
+
+        <?php echo ('<h2> Hello from PHP! </h2>') ?>
+        <?php echo ('<h3> Hello from PHP! Mitchel </h3>') ?>
+        <?php echo ('<h4> Hello from PHP! Niek </h4>') ?>
+        <?php echo ('<h5> Hello from PHP! Owen </h5>') ?>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
 </html>
