a344c0a6a9580134319f310683729d4309be7353
Owen Kuster <owenkuster08@gmail.com>
2026-03-03 12:09:29 +0100

    Added file structures. Added DBCALLS and started on PHP

--- index.php
+++ index.php
@@ -17,10 +17,13 @@
     <main>
         <h1>we gaan hieronder php proberen</h1>
 
-        <?php echo ('<h2> Hello from PHP! </h2>') ?>
-        <?php echo ('<h3> Hello from PHP! Mitchel </h3>') ?>
-        <?php echo ('<h4> Hello from PHP! Niek </h4>') ?>
-        <?php echo ('<h5> Hello from PHP! Owen </h5>') ?>
+        <?php 
+
+        include("dbcalls/menukaart/read.php");
+
+        
+        ?>
+
     </main>
     <footer>
 
